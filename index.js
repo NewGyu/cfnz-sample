5da0d71f73258e552b6afb44e00f2edf1d7b35f7
NewGyu <newgyu@kinoboku.net>
2016-01-27 00:35:19 +0900

    複数のテンプレートを -t オプションで切り替えられるようにした

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 
 var CFNZ = require("cloudformation-z");
 var config = require("config");
-var template = require("./template/index.js");
+var program = require("commander");
 
+program.option("-t --template <templateName>").parse(process.argv);
+if(! program.template) {
+  console.error("template is require");
+  process.exit(1);
+}
+
+var template = require("./template/" + program.template);
 var commander = new CFNZ.EasyCommander(config, template);
-commander.exec(process.argv);
+
+commander.exec(["node","cfnz"].concat(program.args));
