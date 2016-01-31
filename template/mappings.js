2e309a45396083815a8a6dbd36c12c792f5032cf
NewGyu <newgyu@kinoboku.net>
2016-01-31 15:34:19 +0900

    ECSのAMIイメージを2015.09.eに上げた

--- template/mappings.js
+++ template/mappings.js
@@ -1,10 +1,10 @@
 module.exports =  {
   //リージョン毎のECSインスタンスのAMIID管理
   "Region2EcsAMI" : {
-    "us-east-1"      : { "AMIID" : "ami-6ff4bd05" },
-    "us-west-2"      : { "AMIID" : "ami-313d2150" },
-    "eu-west-1"      : { "AMIID" : "ami-8073d3f3" },
-    "ap-northeast-1" : { "AMIID" : "ami-6ca38b02" },
-    "ap-southeast-2" : { "AMIID" : "ami-00e7bf63" }
+    "us-east-1"      : { "AMIID" : "ami-e9724c87" },
+    "us-west-2"      : { "AMIID" : "ami-ec75908c" },
+    "eu-west-1"      : { "AMIID" : "ami-13f84d60" },
+    "ap-northeast-1" : { "AMIID" : "ami-e9724c87" },
+    "ap-southeast-2" : { "AMIID" : "ami-83af8ae0" }
   },
 }
