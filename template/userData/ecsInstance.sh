7cac1829215907a9c9881b7d39bbf7315fe202a1
NewGyu <newgyu@kinoboku.net>
2016-01-31 15:35:15 +0900

    logging-driver=fluentdは明示的にecs-agentに設定しないとあかんらしい。

--- template/userData/ecsInstance.sh
+++ template/userData/ecsInstance.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -xe
 yum update -y
 echo ECS_CLUSTER={"Ref":"MyECSCluster"}>> /etc/ecs/ecs.config
+echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"json-file\",\"syslog\",\"fluentd\"]>> /etc/ecs/ecs.config
